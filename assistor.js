d51896ccbd1eb203f7a85cbe0c06517e86841d72
gbotemi <hommzmum@gmail.com>
2022-05-26 06:23:12 -0700

    update v1.02

--- assistor.js
+++ assistor.js
@@ -8,8 +8,6 @@ let result = firstRequest / 51
 let result2 = result * 1.22
 let thirdRequest = result2 / secondRequest
 let fourtRequest = result - thirdRequest
-alert("odd1 is =" +thirdRequest)
-alert("odd2 is =" +fourtRequest) 
 firststake.textContent = "STAKE1 = "+ thirdRequest 
 secondstake.textContent = "STAKE2 ="+ fourtRequest
 }
@@ -20,11 +18,8 @@ let multiplied = saverequest * 7
 let multiplied2 = multiplied * 1.22
 let multiplied3 = multiplied2 / ask
 let multiplie4 = multiplied3 - multiplied
-alert("The first odd is" + multiplied3)
-alert("the second odd is" + multiplie4)
 firststake.textContent = "STAKE1 ="+ multiplied3
 secondstake.textContent = "STAKE2 ="+ multiplie4
-
 }
 else if (firstquestion === "3"){
 let account =  Number(prompt("enter account balance"))
@@ -32,8 +27,6 @@ let askodd = Number(prompt("enter one odd"))
 let multiplied5 = account * 1.22
 let divisor = multiplied5 / askodd
 let miunus = account - divisor
-alert(divisor)
-alert(miunus)
 firststake.textContent = "STAKE1 =" + divisor
 secondstake.textContent = "STAKE2 =" + miunus
 }
