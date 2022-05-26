0407f803890dc6286265d417987372ba0acca41a
gbotemi <hommzmum@gmail.com>
2022-05-26 06:06:56 -0700

    updated app

---
+++ assistor.js
@@ -0,0 +1,8 @@
+let firstRequest = Number(prompt("enter account balance"))
+let secondRequest =Number(prompt("enter one odd"))
+let result = firstRequest / 51
+let result2 = result * 1.22
+let thirdRequest = result2 / secondRequest
+let fourtRequest = result - thirdRequest
+alert("odd1 is =" +thirdRequest)
+alert("odd2 is =" +fourtRequest)
